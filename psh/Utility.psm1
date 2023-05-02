a9c715b40cfadcd5cced64eb0fb4032fff197a7c
Lingxi Li <lingxl@microsoft.com>
2023-05-03 04:51:50 +0800

    Add Enable/DisableSysProxy

--- psh/Utility.psm1
+++ psh/Utility.psm1
@@ -18,3 +18,16 @@ function VpnConnected {
     }
     $false
 }
+
+function EnableSysProxy([string]$ServerUrl, [string]$Bypass) {
+    Require (-not (VpnConnected)) "VPN is connected"
+    $RegPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+    if ($ServerUrl) { $null = Set-ItemProperty -Path $RegPath -Name "ProxyServer" -Value $ServerUrl -Type String -Force }
+    if ($Bypass) { $null = Set-ItemProperty -Path $RegPath -Name "ProxyOverride" -Value $Bypass -Type String -Force }
+    $null = Set-ItemProperty -Path $RegPath -Name "ProxyEnable" -Value 1 -Type DWord -Force
+}
+
+function DisableSysProxy() {
+    $RegPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+    $null = Set-ItemProperty -Path $RegPath -Name "ProxyEnable" -Value 0 -Type DWord -Force
+}
