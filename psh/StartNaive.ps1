27f0ffb3afe94c419b597d1bf22d31b6b1322fca
Lingxi Li <lingxl@microsoft.com>
2023-05-03 09:20:24 +0800

    Improve StartNaive error handling

--- psh/StartNaive.ps1
+++ psh/StartNaive.ps1
@@ -9,28 +9,37 @@ Require (Test-Path $CfgPath -PathType Leaf) "'$($CfgPath)' not found"
 $Listen = (Get-Content $CfgPath -Raw | ConvertFrom-Json).listen.Replace("0.0.0.0", "localhost")
 
 EnableSysProxy $Listen "<local>"
-$Process = Start-Process -FilePath $AppPath -ArgumentList $CfgPath -WindowStyle Minimized -PassThru
-Start-Sleep -Seconds 1
+try {
+    $Process = Start-Process -FilePath $AppPath -ArgumentList $CfgPath -WindowStyle Minimized -PassThru
+    Start-Sleep -Seconds 1
 
-$Repo = "github.com/klzgrad/naiveproxy"
-$Feed = "https://$($Repo)/releases/latest"
-$Regex = "(?<=<title>Release ).*?(?= )"
-$VerPath = "naive\version.txt"
-$ZipPath = "naive\naive.zip"
+    $Repo = "github.com/klzgrad/naiveproxy"
+    $Feed = "https://$($Repo)/releases/latest"
+    $Regex = "(?<=<title>Release ).*?(?= )"
+    $VerPath = "naive\version.txt"
+    $ZipPath = "naive\naive.zip"
 
-$Page = curl --proxy $Listen -L $Feed
-"Done fetching latest version number"
-$LatestVer = [Regex]::Match($Page, $Regex).Value
-$AssetName = "naiveproxy-$($LatestVer)-win-x64"
-$CurrentVer = Get-Content -Path $VerPath -Raw -ErrorAction SilentlyContinue
-$Update = $CurrentVer -ne $LatestVer
-if ($Update) {
-    curl --proxy $Listen -L "https://$($Repo)/releases/download/$($LatestVer)/$($AssetName).zip" -o $ZipPath
-    "Done fetching latest release"
+    $Page = curl --proxy $Listen -sL --show-error $Feed
+    Require $? "Failed to fetch latest version number, LASTEXITCODE $($LASTEXITCODE)"
+    "Done fetching latest version number"
+    $LatestVer = [Regex]::Match($Page, $Regex).Value
+    $AssetName = "naiveproxy-$($LatestVer)-win-x64"
+    $CurrentVer = Get-Content -Path $VerPath -Raw -ErrorAction SilentlyContinue
+    $Update = $CurrentVer -ne $LatestVer
+    if ($Update) {
+        curl --proxy $Listen -sL --show-error "https://$($Repo)/releases/download/$($LatestVer)/$($AssetName).zip" -o $ZipPath
+        Require $? "Failed to fetch latest release, LASTEXITCODE $($LASTEXITCODE)"
+        "Done fetching latest release"
+    }
+
+    Wait-Process -Id $Process.Id
 }
-
-Wait-Process -Id $Process.Id
-DisableSysProxy
+finally {
+    if (-not $Process.HasExited) {
+        Stop-Process -Id $Process.Id -Force
+    }
+    DisableSysProxy
+}
 
 if ($Update) {
     $null = (New-Object -ComObject Wscript.Shell).Popup("About to update naive...", 0, "Auto Updater")
