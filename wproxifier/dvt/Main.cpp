a6fc8c4d9d8a6e651c3cc834ecc7f94c2aff973b
Lingxi Li <lingxl@microsoft.com>
2023-05-23 13:06:51 +0800

    [Passthru] Implement concurrency

--- wproxifier/dvt/Main.cpp
+++ wproxifier/dvt/Main.cpp
@@ -1,10 +1,16 @@
 #include "Passthru.hpp"
 
 #include <iostream>
+#include <string>
 
 int main() {
     try {
-        Passthru passthru("false");
+        std::string cmd;
+        Passthru passthru;
+        passthru.Start("false");
+        std::cout << "Press <Enter> to stop.\n";
+        std::getline(std::cin, cmd);
+        passthru.Stop();
     }
     catch (std::exception const& err) {
         std::cout << err.what() << '\n';
--- wproxifier/dvt/Passthru.hpp
+++ wproxifier/dvt/Passthru.hpp
@@ -5,14 +5,17 @@
 #include <format>
 #include <fstream>
 #include <system_error>
+#include <thread>
 #include <tuple>
 #include <vector>
 
 class Passthru {
+
     struct ThreadData {
         ThreadData(UINT packetCount, UINT threadIndex)
-            : packets(WINDIVERT_MTU_MAX * packetCount)
-            , addrs(packetCount) {
+            : packets(WINDIVERT_MTU_MAX* packetCount)
+            , addrs(packetCount)
+        {
             auto path = std::format("Passthru_thread_{}.log", threadIndex);
             log.open(path);
             if (!log) {
@@ -26,7 +29,7 @@ class Passthru {
         }
 
         std::tuple<UINT8*, UINT, WINDIVERT_ADDRESS*, UINT, std::ofstream&> Arms() noexcept {
-            return { 
+            return {
                 packets.data(),
                 UINT(packets.size()),
                 addrs.data(),
@@ -42,35 +45,59 @@ class Passthru {
     };
 
 public:
-    Passthru(char const* filter, UINT threadCount = 4, UINT batchSize = 8) {
+    void Start(char const* filter, UINT threadCount = 4, UINT batchSize = 8) {
         threadData.reserve(threadCount);
-        for (UINT i = 0; i < threadCount; ++i) {
-            threadData.emplace_back(batchSize, i);
+        threads.reserve(threadCount);
+        do {
+            threadData.emplace_back(batchSize, UINT(threadData.size()));
         }
+        while (threadData.size() < threadCount);
         handle.Open(filter, WINDIVERT_LAYER_NETWORK, 0, 0);
-        // TODO: spawn worker threads
+        for (auto& data : threadData) {
+            threads.emplace_back(&Passthru::Process, &handle, &data);
+        }
     }
 
-    void StartProcessing(UINT threadIndex) noexcept {
-        auto const [packets, packetsByteLen, addrs, addrsByteLen, log] = threadData[threadIndex].Arms();
+    void Stop() {
+        handle.Shutdown();
+        for (auto& t : threads) {
+            t.join();
+        }
+        threads.clear();
+        handle.Close();
+        threadData.clear();
+    }
+
+private:
+    static void Process(dvt::Handle const* handle, ThreadData* data) noexcept {
+        auto const [packets, packetsByteLen, addrs, addrsByteLen, log] = data->Arms();
         while (true) {
             UINT packetsRecvedByteLen = 0;
             UINT addrsRecvedByteLen = addrsByteLen;
-            auto [res, error] = handle.Recv(packets, packetsByteLen, &packetsRecvedByteLen, 0, addrs, &addrsRecvedByteLen, INFINITE);
+            auto res = handle->Recv(
+                packets,
+                packetsByteLen,
+                &packetsRecvedByteLen,
+                0,
+                addrs,
+                &addrsRecvedByteLen
+            );
             if (res) {
                 // TODO: log inspection
-                if (!handle.Send(packets, packetsRecvedByteLen, NULL, 0, addrs, addrsRecvedByteLen)) {
+                res = handle->Send(packets, packetsRecvedByteLen, NULL, 0, addrs, addrsRecvedByteLen);
+                if (!res) {
                     // TODO: log error
                 }
             }
             else {
-                if (error == WAIT_IO_COMPLETION) break;
+                auto error = GetLastError();
+                if (error == ERROR_NO_DATA) break;
                 // TODO: log error
             }
         }
     }
 
-private:
     std::vector<ThreadData> threadData;
     dvt::Handle handle;
+    std::vector<std::thread> threads;
 };
