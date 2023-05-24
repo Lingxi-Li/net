79c35319357b060da981056e511883b66c2a239c
Lingxi Li <lingxl@microsoft.com>
2023-05-24 20:34:36 +0800

    [dvt] Add RedirPort

--- wproxifier/dvt/Main.cpp
+++ wproxifier/dvt/Main.cpp
@@ -1,16 +1,21 @@
 #include "Passthru.hpp"
+#include "RedirPort.hpp"
 
 #include <iostream>
 #include <string>
 
+void PlayPassthru() {
+    std::string cmd;
+    Passthru passthru;
+    passthru.Start("loopback == 1 && tcp.DstPort == 8000");
+    std::cout << "Press <Enter> to stop.\n";
+    std::getline(std::cin, cmd);
+    passthru.Stop();
+}
+
 int main() {
     try {
-        std::string cmd;
-        Passthru passthru;
-        passthru.Start("loopback == 1 && tcp.DstPort == 8000");
-        std::cout << "Press <Enter> to stop.\n";
-        std::getline(std::cin, cmd);
-        passthru.Stop();
+        RedirPort();
     }
     catch (std::exception const& err) {
         std::cout << err.what() << '\n';
---
+++ wproxifier/dvt/RedirPort.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <Divert.hpp>
+
+#include <net/constants.hpp>
+#include <net/ipv4_view.hpp>
+#include <net/tcp_view.hpp>
+
+#include <stdex/format.hpp>
+
+#include <array>
+#include <iostream>
+
+inline void RedirPort() {
+    constexpr char filter[] = "loopback == 1 && (tcp.DstPort == 8000 || tcp.SrcPort == 8001)";
+    auto handle = WinDivertOpen(filter, WINDIVERT_LAYER_NETWORK, 0, 0);
+    dvt::Check(handle, dvt::Api::WinDivertOpen);
+    std::array<net::byte_t, net::mtu> packet{};
+    WINDIVERT_ADDRESS addr{};
+    while (true) {
+        UINT recvPacketLen = 0;
+        auto res = WinDivertRecv(handle, packet.data(), UINT(packet.size()), &recvPacketLen, &addr);
+        dvt::Check(res, dvt::Api::WinDivertRecv);
+
+        net::ipv4_view ip{packet.data()};
+        net::tcp_view tcp{ip.payload()}; 
+        
+        stdex::format_to(std::cout, "{} => {}", tcp.src_port(), tcp.dst_port());
+        if (tcp.syn()) std::cout << " SIN";
+        if (tcp.ack()) std::cout << " ACK";
+        if (tcp.fin()) std::cout << " FIN";
+        stdex::format_to(std::cout, " {}\n", net::uint_t(ip.total_len()) - ip.header_len() * 4 - tcp.header_len() * 4);
+
+        do {
+            if (tcp.dst_port() == 8000) {
+                tcp.dst_port() = 8001;
+                break;
+            }
+            if (tcp.src_port() == 8001) {
+                tcp.src_port() = 8000;
+                break;
+            }
+        }
+        while (false);
+        WinDivertHelperCalcChecksums(packet.data(), recvPacketLen, &addr, 0);
+
+        res = WinDivertSend(handle, packet.data(), UINT(packet.size()), NULL, &addr);
+        dvt::Check(res, dvt::Api::WinDivertSend);
+    }
+
+}
