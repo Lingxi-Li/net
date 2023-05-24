8cb3f64b9f201a3ec89bf4bceab07b660c65c515
Lingxi Li <lingxl@microsoft.com>
2023-05-24 17:54:05 +0800

    [dvt] Update test TCP server/client

--- wproxifier/dvt/py/tcp_client.py
+++ wproxifier/dvt/py/tcp_client.py
@@ -1,10 +1,12 @@
 import socket
 
 HOST = 'localhost'
-PORT = 8000
+PORT = int(input("Enter server port: "))
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     while True:
         msg = input('>>')
         s.sendall(msg.encode())
+        reply = s.recv(1024)
+        print(f'<<{reply.decode()}')
--- wproxifier/dvt/py/tcp_server_2.py
+++ wproxifier/dvt/py/tcp_server_2.py
@@ -1,7 +1,7 @@
 import socket
 
 HOST = 'localhost'
-PORT = 8000
+PORT = 8001
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -12,4 +12,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         with conn:
             print(f'Connected by {addr}')
             while data := conn.recv(1024):
-                print(data.decode())
+                msg = data.decode().title()
+                conn.sendall(msg.encode())
