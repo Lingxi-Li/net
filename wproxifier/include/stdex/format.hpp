0d5bfc5af8740d702b787b89bb39b1ea507391d8
Lingxi Li <lingxl@microsoft.com>
2023-05-29 21:38:48 +0800

    [stdex] Add DEFINE_ENUM_FORMATTER

--- wproxifier/include/stdex/format.hpp
+++ wproxifier/include/stdex/format.hpp
@@ -1,9 +1,26 @@
 #pragma once
 
+#include <cstddef>
+
 #include <format>
 #include <iterator>
 #include <ostream>
 #include <utility>
+
+#define STDEX_DEFINE_ENUM_FORMATTER(ns, enum_type, ...)                      \
+    template <>                                                              \
+    struct ::std::formatter<ns::enum_type>: formatter<char const*> {         \
+        constexpr static char const* strs[] = { __VA_ARGS__ };               \
+        auto format(ns::enum_type value, format_context& ctx) const {        \
+            auto idx = size_t(value);                                        \
+            return formatter<char const*>::format(strs[idx], ctx);           \
+        }                                                                    \
+    };                                                                       \
+    namespace ns {                                                           \
+        inline ::std::ostream& operator<<(::std::ostream& os, enum_type v) { \
+            return ::stdex::format_to(os, "{}", v);                          \
+        }                                                                    \
+    }
 
 namespace stdex {
 
--- wproxifier/stdex/format.cpp
+++ wproxifier/stdex/format.cpp
@@ -1,14 +1,76 @@
 #include <stdex/format.hpp>
 #include <catch.hpp>
 
-#include <sstream>
+#include <test_utility.hpp>
 
-using namespace std;
-using namespace stdex;
+namespace {
+
+enum struct pet: unsigned {
+    cat,
+    dog
+};
+
+struct nest {
+    enum struct bird: unsigned {
+        swallow,
+        eagle
+    };
+};
+
+} // unnamed namespace
+
+STDEX_DEFINE_ENUM_FORMATTER(, pet,
+    "cat",
+    "dog"
+);
+
+STDEX_DEFINE_ENUM_FORMATTER(, nest::bird,
+    "swallow",
+    "eagle"
+);
+
+namespace fmt_test {
+
+enum struct direction: unsigned {
+    inbound,
+    outbound
+};
+
+struct outer {
+    struct inner {
+        enum struct name: unsigned {
+            lingxi,
+            echo
+        };
+    };
+};
+
+} // namespace fmt_test
+
+STDEX_DEFINE_ENUM_FORMATTER(fmt_test, direction,
+    "<-",
+    "->"
+);
+
+STDEX_DEFINE_ENUM_FORMATTER(fmt_test, outer::inner::name,
+    "lingxi",
+    "echo"
+);
 
 TEST_CASE("format_to") {
-    ostringstream os;
-    auto& ref = format_to(os, "{:x}", 65535);
+    std::ostringstream os;
+    auto& ref = stdex::format_to(os, "{:x}", 65535);
     REQUIRE(os.str() == "ffff");
     REQUIRE(&ref == &os);
+}
+
+TEST_CASE("DEFINE_ENUM_FORMATTER") {
+    REQUIRE(str_via_format(pet::cat) == "cat");
+    REQUIRE(str_via_ostream(pet::dog) == "dog");
+    REQUIRE(str_via_format(nest::bird::swallow) == "swallow");
+    REQUIRE(str_via_ostream(nest::bird::eagle) == "eagle");
+    REQUIRE(str_via_format(fmt_test::direction::inbound) == "<-");
+    REQUIRE(str_via_ostream(fmt_test::direction::outbound) == "->");
+    REQUIRE(str_via_format(fmt_test::outer::inner::name::lingxi) == "lingxi");
+    REQUIRE(str_via_ostream(fmt_test::outer::inner::name::echo) == "echo");
 }
